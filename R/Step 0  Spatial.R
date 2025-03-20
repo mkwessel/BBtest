bc72548b6595b3eaaa9c11571d7e25a061afe828
Travis Hinkelman <thinkelman@esassoc.com>
2025-03-20 11:45:04 -0700

    Fix bug in Step 0 Spatial.R

--- R/Step 0  Spatial.R
+++ R/Step 0  Spatial.R
@@ -6,7 +6,8 @@ library(tidyr)
 # there is also "ENRY7" and WBID 3226G3 that is bisected by the northern boundary 
 enrs = c("ENRH1", "ENRH3", "ENRH4", "ENRH5", "ENRH6", "ENRH7", "ENRH8", "ENRH9", "ENRY7")
 
-# these polygons are based on WBID but not combining them to ENRs for now
+bb_nnc <- readRDS(file.path("Data", "BB_NNC.rds"))
+
 bb_nnc_sub <- readRDS(file.path("Data", "BB_NNC.rds")) |> 
   filter(ENR %in% enrs) |> 
   select(Segment, ENR, WBID, ENR, TN, TP, CHLAC)
@@ -22,6 +23,6 @@ stations <- read_sf(file.path("Data", "IWR_Stations_Run66.shp")) |>
   st_transform(crs = 4326) |>
   select(sta = STATION_ID, WBID = WATERBODY_, lat = LATITUDE, lon = LONGITUDE, geometry) |> 
   filter(WBID %in% unique(bb_nnc_sub$WBID)) |> 
-  # add ENR to stations based on WBID
-  left_join(select(st_drop_geometry(bb_nnc_sub), WBID, ENR, Segment))
+  st_intersection(select(bb_nnc_sub, ENR))
 saveRDS(stations, file.path("Data", "BB_ENR_Stations.rds"))
+
